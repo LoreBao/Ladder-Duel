29712608300b2da07edb1d67e03b384b360f2ef0
kevin.louis.1170@gmail.com <root@Hloe.localdomain>
2026-01-30 16:01:39 +0800

    Implement GamePage component and enhance game state management with action handling

--- packages/shared/src/engine/reduce.ts
+++ packages/shared/src/engine/reduce.ts
@@ -9,7 +9,85 @@ export function reduce(
     action: GameAction,
     deps: EngineDeps=createDefaultDeps()
 ): GameState{
-    if(state.winner){
+    if(state.winner&&action.type!=="RESET"){
         return appendLog(state,"Game Has Ended!");
+    }
+    if("player" in action&&action.player!==state.currentPlayer){
+        return appendLog(state,`Action ${action.player} Didn't Match With ${state.currentPlayer}`);
+    }
+
+    switch(action.type){
+        case "RESET":{
+            return createInitalState();
+        }
+        
+        case "PLAY_CARD":{
+            return appendLog(state,"Incomplete");
+        }
+
+        case "SKIP_CARD":{
+            let newState=state;
+            newState.phase="ROLL";
+            return appendLog(newState,"Skip Card");
+        }
+
+        case "ROLL_DICE":{
+            const diceValue=deps.rollDice();
+            let newState=state;
+            newState.lastroll={
+                roller:state.currentPlayer,
+                value:diceValue
+            }
+            newState.phase="RESOLVE";
+            return appendLog(newState,`${newState.currentPlayer} Has Rolled a Value of ${diceValue}`);
+        }
+
+        case "RESOLVE_ROLL":{
+            if(!state.lastroll){
+                let newState=state;
+                newState.phase="DRAW"
+                return appendLog(newState, "Error: Without Essential Last Roll");
+            }
+
+            const target=getOpponent(state.lastroll.roller);
+            const delta=-state.lastroll.value
+            
+            const newPosition=clampPosition(state.players[target].position+delta)
+            let newState=state;
+            newState.players[target].position=newPosition;
+            newState.phase="DRAW"
+            newState=appendLog(newState, `${target}->${newPosition}`)
+            
+            if(newPosition<=0){
+                newState.winner=getOpponent(target);
+            }
+
+            return appendLog(newState, `Winner is ${newState.winner}`)
+        }
+
+        case "DRAW_CARD":{
+            if(state.deck.length===0){
+                let newState=state;
+                newState.phase="END"
+                return appendLog(newState, "End Turn")
+            }
+            const drawCard=state.deck.pop()  
+            let newState=state;
+            if(drawCard!==undefined){
+                 newState.players[state.currentPlayer].hand = [...newState.players[state.currentPlayer].hand, drawCard]
+            }
+
+            newState.phase="END";
+            return appendLog(newState,`${newState.currentPlayer} has drawn ${drawCard}!`);
+        }
+
+        case "END_TURN":{
+            let newState=state;
+            newState.turn+=1;
+            newState.phase="Action"
+            newState.currentPlayer=getOpponent(newState.currentPlayer);
+            newState.lastroll=undefined;
+            return appendLog(newState, `End of Turn, Changing Player ${newState.currentPlayer}`);
+        }
     }
 }
