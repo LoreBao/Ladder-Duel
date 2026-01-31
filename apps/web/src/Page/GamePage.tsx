839f21d4b5e8f3df501273e09b0769497088dcf0
kevin.louis.1170@gmail.com <root@Hloe.localdomain>
2026-01-31 20:22:46 +0800

    Refactor GamePage component layout with additional UI elements

--- apps/web/src/Page/GamePage.tsx
+++ apps/web/src/Page/GamePage.tsx
@@ -3,6 +3,25 @@ import React from "react";
 function GamePage(){
     return(
         <>
+        <button>Picker Button</button>
+        <div className="Gallery">Gallery</div>
+        <p>Turn</p>
+        <div className="p1">
+            <h1>P1</h1>
+
+        </div>
+
+        <div className="p2">
+            <h1>P2</h1>
+        </div>
+
+        <div className="operationBtns">
+            <h1>Operation Buttons</h1>
+        </div>
+
+        <div className="log">
+            <h1>Log Panel</h1>
+        </div>
         </>
     )
 }
