34ff1463340b584c85309afdf12fbd48f98cae16
kevin.louis.1170@gmail.com <root@Hloe.localdomain>
2026-03-14 17:42:26 +0800

    v1 of homepage

---
+++ apps/web/src/Page/homepage.tsx
@@ -0,0 +1,39 @@
+function footer(){
+    return (
+        <>
+        <div id="footer">
+            <h6>Share your ideas with me here!</h6>
+            <p>Link: https://github.com/LoreBao/Ladder-Duel</p>
+            <p>Email: frajola484@gmail.com</p>
+        </div>
+        </>
+
+    )
+}
+
+function selfIntro(){
+    return(
+        <>
+        <div id="si">
+            <h3>Hello! I'm Kevin</h3>
+            <p>These are my two pretty weird games I made, I love making strange ideas into reality</p>
+        </div>
+        </>
+    )
+}
+
+function GameEntry(){
+    return(
+        <>
+        <div id="ladder">
+            <p>Quick, Intense Two Player Game!</p>
+            <button>Ladder Duel (Newest Release)</button>
+        </div>
+
+        <div id="clicker">
+            <p>Chaotic Fun and Randomness</p>
+            <button>Jerry Clicker (First Game!)</button>
+        </div>
+        </>
+    )
+}
