9890ec50aa126f353c2987d324e43b6d06e4bd7c
kevin.louis.1170@gmail.com <root@Hloe.localdomain>
2026-04-12 16:01:23 +0800

    feat: Implement HomePage component with footer, self-introduction, and game entries

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,17 +1,21 @@
+import {Routes, Route, HashRouter} from "react-router-dom";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { GamePage } from './Page/GamePage'
+import { GamePage } from './Page/GamePagePractice'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <GamePage/>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={GamePage}/>
+        <Route path="/play" element={GamePage}/>
+      </Routes>
+    </HashRouter>
   )
 }
 
